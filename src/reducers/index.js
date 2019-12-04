46af7ef5eb6ea9a2463ddcf470dffec8242f2bee
Hector Rodriguez Fornies <torhector2@gmail.com>
2019-12-04 11:44:17 +0700

    Create store and wrapp App with Provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
-import { authedUser } from './authedUser'
-import { users } from './users'
-import { tweets } from './tweets'
+import authedUser from './authedUser'
+import users from './users'
+import tweets from './tweets'
 
 export default combineReducers({
     authedUser,
