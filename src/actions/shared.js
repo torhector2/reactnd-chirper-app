350177d87682cf93dca9bf86465187ec66fb43b2
Hector Rodriguez Fornies <torhector2@gmail.com>
2019-12-05 11:05:04 +0700

    Download initial data from App
    Fix issue where tweets and users weren't processed properly in the handleInitialData

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -7,7 +7,7 @@ const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
     return (dispatch) => {
-        return getInitialData().then((users, tweets) => {
+        return getInitialData().then(({users, tweets}) => {
             dispatch(receiveTweets(tweets))
             dispatch(receiveUsers(users))
             dispatch(setAuthedUser(AUTHED_ID))
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,5 @@ class App extends Component {
   }
 }
 
-export default App
+//no need to pass state, we just want dispatch so there's no need to pass any param to connect()
+export default connect()(App)
