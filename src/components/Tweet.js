09ef833bb16919edac3622d88aa371f775bc814c
Hector Rodriguez Fornies <torhector2@gmail.com>
2019-12-06 22:22:21 +0700

    Perform like tweet action

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,12 +4,19 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
+import { handleToggleTweet } from '../actions/tweets'
 
 class Tweet extends Component {
     handleLike = (e) => {
         e.preventDefault()
 
-        //todo: handle like tweet
+        const { dispatch, tweet, authedUser } = this.props
+
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser
+        }))
     }
 
     toParent = (e, id) => {
